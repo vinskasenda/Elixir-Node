1a7c52550ff13c1bd197542e6e83e6f645e1c857
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-09-10 19:56:12 +0700

    Create icon.sh

---
+++ icon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}"
+echo " __     ___ _    _ _            _     _ "
+echo " \ \   / (_) | _(_) |_ ___  ___| |__ (_)"
+echo "  \ \ / /| | |/ / | __/ _ \/ __| '_ \| |"
+echo "   \ V / | |   <| | || (_) \__ \ | | | |"
+echo "    \_/  |_|_|\_\_|\__\___/|___/_| |_|_|"
+echo ""
+echo "Join Telegram Group : https://t.me/AirdropInsiderID"
+echo -e "${NC}"
